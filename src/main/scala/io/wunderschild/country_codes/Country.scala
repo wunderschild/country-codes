0fc266737b2e7e08b9260d55376d359ceec8ca92
GoshaQ <g.emelyanov@innopolis.ru>
2020-05-02 22:16:46 +0300

    Resolve non-deterministic nationality resolution

--- src/main/scala/io/wunderschild/country_codes/Country.scala
+++ src/main/scala/io/wunderschild/country_codes/Country.scala
@@ -3,7 +3,7 @@ package io.wunderschild.country_codes
 case class Country(
     alpha2: String,
     alpha3: String,
-    nationality: String,
+    nationality: Option[String] = None,
     sovereignty: Option[String] = None,
     officialName: String,
     otherNames: Option[Seq[String]] = None)
--- src/main/scala/io/wunderschild/country_codes/LookupTable.scala
+++ src/main/scala/io/wunderschild/country_codes/LookupTable.scala
@@ -5,15 +5,23 @@ package io.wunderschild.country_codes
  * @param countries list of countries loaded from the disk
  */
 class LookupTable(countries: Seq[Country], indexedFields: Seq[String]) extends Serializable {
+  private def toOption(anyVal: Any): Option[Any] = {
+    anyVal match {
+      case v @ Some(null) => None
+      case v @ Some(_: Any) => v
+      case _ => Option(anyVal)
+    }
+  }
+
   private lazy val holder: Map[Int, Country] = {
     val fieldsLens = classOf[Country].getDeclaredFields.filter(f => indexedFields.contains(f.getName))
     countries.foldLeft(Map.empty[Int, Country]) { (lookup, country) =>
       val values = fieldsLens.foldLeft(Seq.empty[Any]) { (values, field) =>
         field.setAccessible(true)
-        field.get(country) match {
-          case Some(null) => values
+        toOption(field.get(country)) match {
           case Some(v: Seq[Any]) => v ++ values
-          case v: Any => v +: values
+          case Some(v: Any) => v +: values
+          case None => values
         }
       }
       val hashes = values.map(name => name.toString.toLowerCase.hashCode)
