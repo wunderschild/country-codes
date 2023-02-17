e0e2cd34744fff36fbf45f4776fc5a3bc5acfd50
Gosha Emelyanov <g.emelyanov@schwarzthal.com>
2023-02-17 15:45:35 +0300

    Bump versions

--- country-codes-rb/lib/country_codes/version.rb
+++ country-codes-rb/lib/country_codes/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 module CountryCodes
-  VERSION = '0.1.5'
-  LIB_VERSION = '0.0.10'
+  VERSION = '0.1.6'
+  LIB_VERSION = '0.0.11'
 end
--- country-codes/build.sbt
+++ country-codes/build.sbt
@@ -1,5 +1,5 @@
 name := "country-codes"
-version := "0.0.10"
+version := "0.0.11"
 
 val jacksonVersion = "2.13.5"
 
